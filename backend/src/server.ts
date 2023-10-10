6f869f0f12685c707902dc68f88597384b533535
felipemattosv <felipe.vanetti@gmail.com>
2023-10-09 23:03:13 -0300

    add: generate-ai-completion route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import { fastifyCors } from '@fastify/cors';
 import { getAllPromptsRoute } from './routes/get-all-prompts';
 import { uploadVideoRoute } from './routes/upload-video';
 import { createTranscriptionRoute } from './routes/create-transcription';
+import { generateAiCompletionRoute } from './routes/generate-ai-completion';
 
 const app = fastify();
 
@@ -14,6 +15,7 @@ app.register(fastifyCors, {
 app.register(getAllPromptsRoute);
 app.register(uploadVideoRoute);
 app.register(createTranscriptionRoute);
+app.register(generateAiCompletionRoute);
 
 app.listen({
   port: 3333,
